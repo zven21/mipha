5de68b9071848ded0efd597b01b19a731a750509
Zven21 <andzven@gmail.com>
2018-07-10 22:26:32 +0800

    FIX

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mipha.Mixfile do
     [
       app: :mipha,
       version: "0.0.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,

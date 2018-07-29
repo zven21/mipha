a5293aee8e35e5249d6b3076fb97b78f4e087a86
Zven21 <andzven@gmail.com>
2018-07-29 22:51:40 +0800

    add ransack_search_input

--- lib/mipha_web/ransack.ex
+++ lib/mipha_web/ransack.ex
@@ -9,4 +9,18 @@ defmodule MiphaWeb.Ransack do
   def ransack_pagination_links(paginator) do
     Paginate.ransack_pagination_links(paginator)
   end
+
+  @doc """
+  装饰 paginate 参数，添加 params 和 path_info
+  """
+  def decorator_paginate(conn, paginate) do
+    Map.merge(paginate, %{path_info: conn.path_info, params: conn.params})
+  end
+
+  @doc """
+  搜索 Input 装饰器
+  """
+  def ransack_search_input(conn, field, opts) do
+    Form.ransack_search_input(conn, field, opts)
+  end
 end
--- lib/mipha_web/views/view_helpers.ex
+++ lib/mipha_web/views/view_helpers.ex
@@ -2,20 +2,12 @@ defmodule MiphaWeb.ViewHelpers do
   @moduledoc """
   View helpers for global funcs we find useful.
   """
-
   import Phoenix.HTML, only: [raw: 1]
   alias Mipha.Markdown
 
   def markdown(body) do
     body
     |> Markdown.render
-    |> raw
-  end
-
-  @doc """
-  装饰 paginate 参数，添加 params 和 path_info
-  """
-  def decorator_paginate(conn, paginate) do
-    Map.merge(paginate, %{path_info: conn.path_info, params: conn.params})
+    |> raw()
   end
 end

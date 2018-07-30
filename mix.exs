6bb1f142d401ac3f8cfe9a46dda3ce405e29b65e
Zven21 <andzven@gmail.com>
2018-07-30 10:56:05 +0800

    change favicon.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Mipha.Mixfile do
   def application do
     [
       mod: {Mipha.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :captcha]
     ]
   end
 

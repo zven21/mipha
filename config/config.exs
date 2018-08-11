7a985b0ecfdc73c4a1ac00492d5ad41f86d38c1b
ZhenyuanLau <zhenyuanlau@icloud.com>
2018-08-11 17:43:29 +0800

    Setting Gettext Default locale

--- config/config.exs
+++ config/config.exs
@@ -50,6 +50,8 @@ config :sentry,
 config :turbo_ecto, Turbo.Ecto,
   repo: Mipha.Repo
 
+config :gettext, :default_locale, "zh"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"

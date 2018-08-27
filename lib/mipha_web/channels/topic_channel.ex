e909346bea88ff8c7459242e3ef4afbe4de7f068
Zven21 <andzven@gmail.com>
2018-08-27 22:37:54 +0800

    FIX

--- lib/mipha_web/channels/topic_channel.ex
+++ lib/mipha_web/channels/topic_channel.ex
@@ -1,9 +1,33 @@
 defmodule MiphaWeb.TopicChannel do
-  @moduledoc false
+  @moduledoc """
+  Topic Channel
+  """
 
   use MiphaWeb, :channel
 
-  def join("topic:" <> topic_id, _params, socket) do
-    {:ok, %{channel: "topic:#{topic_id}"}, assign(socket, :topic_id, topic_id)}
+  def join("topic:" <> topic_id, payload, socket) do
+    if authorized?(payload) do
+      {:ok, %{channel: "topic:#{topic_id}"}, assign(socket, :topic_id, topic_id)}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (topic:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
   end
 end

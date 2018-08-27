b6e45ff636c503785f0f8a924be459ffaadff69f
Zven21 <andzven@gmail.com>
2018-08-27 22:11:27 +0800

    Heroku response

--- lib/mipha_web/channels/user_socket.ex
+++ lib/mipha_web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule MiphaWeb.UserSocket do
   channel "topic:*", MiphaWeb.TopicChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can

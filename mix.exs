4092a47eb81fe427f50e92ddbf27f8bb5fed70eb
zven <andzven@gmail.com>
2020-09-16 13:35:04 +0800

    test 1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mipha.Mixfile do
     [
       app: :mipha,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
